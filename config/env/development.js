811f3a755ac3a7637bb43f2e6ee7a45b738cdca2
hkmgosu <enfer.sur.moi@gmail.com>
2016-04-12 20:36:17 -0300

    removing trash

---
+++ config/env/development.js
@@ -0,0 +1,6 @@
+module.exports = {
+	MONGOLAB_URI: 'mongodb://admin:mobicongress0199@ds059692.mongolab.com:59692/mobicongress',
+	PARSE_ACCOUNT_USER: 'evolutionaimagos@gmail.com',
+	PARSE_ACCOUNT_PASSWORD: 'mobicongress0199',
+	sessionSecret: 'developmentSessionSecret'
+};
